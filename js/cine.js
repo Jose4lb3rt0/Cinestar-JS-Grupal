4cda74b08899bc20fae9889738afe110411ee6bc
José Salcedo <josealberto3200@gmail.com>
2024-05-02 17:14:20 -0500

    Commit 2 - Cine

--- js/cine.js
+++ js/cine.js
@@ -1,28 +1,44 @@
-getCine = async() =>{
-    id = new URLSearchParams(window.location.search).get(id);
-    data = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}`);
-    data1 = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}/tarifas`);
-    data2 = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}/peliculas`);
+const getCine = async() =>{
+    const id = new URLSearchParams(window.location.search).get(`id`);
+    const data = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}`);
+    const data1 = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}/tarifas`);
+    const data2 = await fetch(`https://oaemdl.es/cinestar_sweb_php/cines/${id}/peliculas`);
 
     if(data.status = 200){
-        cine = await data.json();
-        tarifas = await data1.json();
-        peliculas = await data2.json();
-    }
+        const cine = await data.json();
+        const tarifas = await data1.json();
+        const peliculas = await data2.json();
+    
+    let htmlTarifas = '';
+    tarifas.forEach(tarifa => {
+        htmlTarifas += `
+        <div class="fila">
+            <div class="celda-titulo">${tarifa.DiasSemana}</div>
+            <div class="celda">${tarifa.Precio}</div>
+        </div>
+        `
+    });
+
+    let htmlPeliculas = '';
+    peliculas.forEach(pelicula =>{
+        htmlPeliculas += `
+        <div class="fila">
+            <div class="celda-titulo">${pelicula.Titulo}</div>
+            <div class="celda">${pelicula.Horarios}</div>
+        </div>
+        `
+    });
 
     let html = `
-        <h2>Cinestar Excelsior</h2>
+        <h2>${cine.RazonSocial}</h2>
         <div class="cine-info">
             <div class="cine-info datos">
-                <p>${cine.RazonSocial}</p>
+                <p>${cine.Direccion}</p>
                 <p>Teléfono: ${cine.Telefonos}</p>
                 <br/>
                 <div class="tabla">
 
-                    <div class="fila">
-                        <div class="celda-titulo">${cine.DiasSemana}</div>
-                        <div class="celda">${cine.Precio}</div>
-                    </div>
+                ${htmlTarifas}
 
                 </div>
                 <div class="aviso">
@@ -33,10 +49,9 @@ getCine = async() =>{
             <br/><br/><h4>Los horarios de cada función están sujetos a cambios sin previo aviso.</h4><br/>
             <div class="cine-info peliculas">
                 <div class="tabla">
-                    <div class="fila">
-                        <div class="celda-titulo">${cine.Titulo}</div>
-                        <div class="celda">${cine.Horarios}</div>
-                    </div>
+                    
+                ${htmlPeliculas}
+
                 </div>
             </div>
         </div>
@@ -52,6 +67,7 @@ getCine = async() =>{
         </div>    
     `
     document.getElementById('contenido-interno').innerHTML=html;
+    }
 }
 
 /* 
